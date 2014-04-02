76d771a8519bf50b84090844895a86a9f2824c89
Jodie Belgrave <rmb1993@gmail.com>
2014-04-02 15:32:38 -0400

    STSC include SC internal IP to allow servers to reconnect incase of crash

--- src/main/java/com/rmb938/jedis/net/command/server/NetCommandSTSC.java
+++ src/main/java/com/rmb938/jedis/net/command/server/NetCommandSTSC.java
@@ -4,7 +4,6 @@ import com.google.common.base.Preconditions;
 import com.rmb938.jedis.net.NetChannel;
 import com.rmb938.jedis.net.NetCommand;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,17 +12,22 @@ public class NetCommandSTSC extends NetCommand {
 
     private final static Logger logger = Logger.getLogger(NetCommandSTSC.class.getName());
 
+    private final String toServerController;
     private final int fromServerPort;
 
     /**
      * A Command that goes from a server to its server controller
      * @param name - name of the command
+     * @param toServerController - the internal IP of the server controller
      * @param fromServerPort - serverName.port
      */
-    public NetCommandSTSC(String name, int fromServerPort) {
+    public NetCommandSTSC(String name, String toServerController, int fromServerPort) {
         super(name, NetChannel.SERVER_TO_SERVER_CONTROLLER);
+        Preconditions.checkNotNull(toServerController, "Net Command STSC toServerController cannot be null");
         Preconditions.checkNotNull(fromServerPort, "Net Command STSC fromServerPort cannot be null");
         Preconditions.checkArgument(fromServerPort > 0, "Net Command STSC fromServerPort must be greater than 0");
+        Preconditions.checkArgument(toServerController.trim().isEmpty() == false, "Net Command STSC toServerController cannot be empty");
+        this.toServerController = toServerController;
         this.fromServerPort = fromServerPort;
         buildJSON();
     }
@@ -31,6 +35,7 @@ public class NetCommandSTSC extends NetCommand {
     public void buildJSON() {
         try {
             getJsonObject().put("from", fromServerPort);
+            getJsonObject().put("to", toServerController);
         } catch (JSONException e) {
             logger.log(Level.SEVERE, null, e);
         }
