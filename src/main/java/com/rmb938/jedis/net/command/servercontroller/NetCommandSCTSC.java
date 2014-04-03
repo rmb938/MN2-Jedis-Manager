b18f36a62478dfaeb2d65e53a333f1c23ebea8e2
Jodie Belgrave <rmb1993@gmail.com>
2014-04-03 13:07:36 -0400

    added SCTSC channel

---
+++ src/main/java/com/rmb938/jedis/net/command/servercontroller/NetCommandSCTSC.java
@@ -0,0 +1,44 @@
+package com.rmb938.jedis.net.command.servercontroller;
+
+import com.google.common.base.Preconditions;
+import com.rmb938.jedis.net.NetChannel;
+import com.rmb938.jedis.net.NetCommand;
+import org.json.JSONException;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class NetCommandSCTSC extends NetCommand {
+
+    private final static Logger logger = Logger.getLogger(NetCommandSCTS.class.getName());
+
+    private final String toServerController;
+    private final String fromServerController;
+
+    /**
+     * A Command that goes from a server controller to a server
+     * @param name - the name of the command
+     * @param fromServerController - the server controller's internal ip address
+     * @param toServerController - the server controller's internal ip address
+     *                     If toServerController == * command is a broadcast to all servers controllers
+     */
+    public NetCommandSCTSC(String name, String fromServerController, String toServerController) {
+        super(name, NetChannel.SERVER_CONTROLLER_TO_BUNGEE);
+        Preconditions.checkNotNull(fromServerController, "Net Command SCTS fromServerController cannot be null");
+        Preconditions.checkNotNull(toServerController, "Net Command SCTS toServerController cannot be null");
+        Preconditions.checkArgument(fromServerController.trim().isEmpty() == false, "Net Command SCTS fromServerController cannot be empty");
+        Preconditions.checkArgument(toServerController.trim().isEmpty() == false, "Net Command SCTS toServerController cannot be empty");
+        this.fromServerController = fromServerController;
+        this.toServerController = toServerController;
+        buildJSON();
+    }
+
+    public void buildJSON() {
+        try {
+            getJsonObject().put("from", fromServerController);
+            getJsonObject().put("to", toServerController);
+        } catch (JSONException e) {
+            logger.log(Level.SEVERE, null, e);
+        }
+    }
+}
